5b684cd4f1a6cfe4790f140c370ab442c6f767cf
Kim Hyounwoo <25_21@bssn.hs.kr>
2025-05-07 17:54:37 +0900

    y

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
-st.title("this is title")   
-st.header("this is header")
-st.subheader("this is subheader")
+col1, col2 = st.columns([2,3])
+
+with col1:
+    st.title("here is colunm1")
+with col2:
+    st.title("here is colunm2")
+    st.checkbox("this is a checkbox in colunm2")
+
+col1.subheader("I am a subheader!!")
+col2.checkbox("this is checkbox in colunm2")
